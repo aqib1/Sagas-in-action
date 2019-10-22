5450f37fc77e539291f220abb000495ce7b76755
Aqib Javed <aqibbutt3078@gmail.com>
2019-10-22 13:28:04 +0500

    Init project

---
+++ sagas-in-action/src/main/java/com/example/axon/SagasInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.axon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagasInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagasInActionApplication.class, args);
+	}
+
+}
---
+++ sagas-in-action/src/test/java/com/example/axon/SagasInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.axon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagasInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
